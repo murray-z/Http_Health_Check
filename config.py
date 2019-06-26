4ad17122bc2f0259f99c292dce431f11c4ec46eb
zhangfazhan <37731167+zhangfazhan@users.noreply.github.com>
2019-06-26 14:11:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ config = {
     "to_email": ["xxxxxx@qq.com"],        # 收件邮箱
     "check_Intervals": 3600,              # 检查时间间隔，单位：秒
     "check_total": 100,                   # 每次检查调用接口次数
-    "error_num": 5,                       # 预警阈值，即总检查次数超过error_num发送邮件
+    "error_num": 5,                       # 预警阈值，即总检查次数中错误大于等于error_num发送邮件
     "server_name": "my test server"       # 服务名称
 }
 
@@ -25,3 +25,6 @@ def get_response_code():
         return response.status_code
     except :
         return 'error'
+    
+    
+    
